e211dccbd58411f418650b9dd2de06fe705ab5f2
Jakub Kus <113033886+KubaK1P@users.noreply.github.com>
2023-10-25 10:20:25 +0200

    dziala wszytko chyba

--- szyfry/main.py
+++ szyfry/main.py
@@ -61,7 +61,7 @@ class Data:
         self.data = result
         return result
 
-    def atbash_decode(self):
+    def atbash_cipher_decode(self):
         alphabet = 'abcdefghijklmnopqrstuvwxyz'
         atbash_map = {alphabet[i]: alphabet[25 - i] for i in range(26)}
         if not self.encoded:
@@ -78,13 +78,57 @@ class Data:
 
 
 def main():
-    dane = Data("sprawdzam czemu to nie jest jak ma byc", "atbash")
-    print(dane.data)
-    dane.caesars_cipher_decode()
-    print(dane.data)
-
+    working = True
+    lista = []
+    print("1. Dodaj do bazy \n2. Usuń z bazy \n3.Odszyfruj \n4.Pokaz baze \n5.Wyjdz")
+    while working:
+        try:
+            choice = int(input("Podaj opcje: "))
+        except ValueError:
+            print("PodajPrawidłową watrość")
+            continue
+        if choice == 1:
+            dane = str(input("Podaj dane do zaszyfrowania: "))
+            szyfr = str(input("Jaki szyfr: "))
+            if szyfr == "caesars":
+                lista.append(Data(dane, "caesars"))
+                print(lista[-1].data)
+                continue
+            elif szyfr == "atbash":
+                lista.append(Data(dane, "atbash"))
+                print(lista[-1].data)
+                continue
+            else:
+                print("Podałeś zły szyfr")
+                continue
+        elif choice == 2:
+            idx = int(input("Podaj miejsce do usuniecia: "))
+            try:
+                lista.pop(idx-1)
+                print("Usunięto")
+            except IndexError:
+                print("Index nie jest w liście")
+                continue
+        elif choice == 3:
+            idx1 = int(input("Podaj miejsce do odszyfrowania: "))
+            szyfr1 = str(input("Jaki szyfr: "))
+            try:
+                if szyfr1 in ["caesars", "atbash"]:
+                    if szyfr1 == "caesars":
+                        lista[idx1-1].caesars_cipher_decode()
+                        print(lista[idx1-1].data)
+                    else:
+                        lista[idx1-1].atbash_cipher_decode()
+                    print(lista[idx1-1].data)
+                    continue
+            except IndexError:
+                print("Index nie w liście")
+        elif choice == 4:
+            for i in lista:
+                print(f'{i.data}', end="\n")
+                continue
+        elif choice == 5:
+            working = False
 
 if __name__ == '__main__':
     main()
-
-
